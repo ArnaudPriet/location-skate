50a245a121a773f259c02e8fd3f041321da89dcc
ArnaudPriet <arnaud.priet1@gmail.com>
2021-09-09 13:14:27 +0200

    chatroom

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,12 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
+import "bootstrap";
+
+import { initChatroomCable } from '../channels/chatroom_channel';
+
+document.addEventListener('turbolinks:load', () => {
+  initChatroomCable();
+});
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+Chatroom.destroy_all
+Renting.destroy_all
 Skate.destroy_all
 User.destroy_all
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-anna = User.create(name: 'Anna', email: 'anna@lewagon.com', password: '123456')
-arnaud = User.create(name: 'Arnaud', email: 'arnaud@lewagon.com', password: '123456')
-chris = User.create(name: 'Chris', email: 'chris@lewagon.com', password: '123456')
-genevieve = User.create(name: 'Genevieve', email: 'genevieve@lewagon.com', password:'123456')
+anna = User.create(name: 'Anna', nickname: "nana", email: 'anna@lewagon.com', password: '123456')
+arnaud = User.create(name: 'Arnaud', nickname: "nono", email: 'arnaud@lewagon.com', password: '123456')
+chris = User.create(name: 'Chris', nickname: "cricri", email: 'chris@lewagon.com', password: '123456')
+genevieve = User.create(name: 'Genevieve', nickname: "gigi", email: 'genevieve@lewagon.com', password:'123456')
 
 skate1 = Skate.create(name: 'fusion', description: 'anna@lewagon.com', price: 12.34)
 skate2 = Skate.create(name: 'atom', description: 'arnaud@lewagon.com', price: 1.23)
@@ -22,3 +23,5 @@ skate4 = Skate.create(name: 'soleil', description: 'genevieve@lewagon.com', pric
 
 renting1 = Renting.create(date: '08/09/21', user: anna, skate: skate1)
 renting2 = Renting.create(date: '09/12/21', user: arnaud, skate: skate3)
+
+Chatroom.create!(name: "général")
